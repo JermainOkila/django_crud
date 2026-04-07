5b0faeef16094c9bd6263f79ea0bd6bcfc31054d
JermainOkila <jermainokila@gmail.com>
2026-04-07 09:20:42 -0400

    added database config

--- mediumcrud/settings.py
+++ mediumcrud/settings.py
@@ -73,11 +73,12 @@ WSGI_APPLICATION = 'mediumcrud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+
 }
 
 
@@ -119,4 +120,10 @@ STATIC_URL = 'static/'
 import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
+
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
 
--- mediumcrud/urls.py
+++ mediumcrud/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path('', include('myapp.urls')),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
